9d12eb0f08092d6678b83ee441f41c96f8cbb7a9
Dyskal <yoshilexis@gmail.com>
2020-05-05 18:09:40 +0200

    update project

--- src/main/java/fr/tatscher/combatminions/CombatMinions.java
+++ src/main/java/fr/tatscher/combatminions/CombatMinions.java
@@ -13,7 +13,7 @@ public class CombatMinions {
     public static final String VERSION = "0.1-alpha";
 
     private static Logger logger;
-
+    //oui
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
     {
