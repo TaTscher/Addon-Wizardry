d013053f192994d7daa6c1ffa7c47ec341a1a908
Dyskal <yoshilexis@gmail.com>
2020-05-05 17:47:34 +0200

    update project

--- src/main/java/fr/tatcher/combatminions/CombatMinions.java
+++ src/main/java/fr/tatcher/combatminions/CombatMinions.java
@@ -1,17 +1,15 @@
-package com.example.examplemod;
+package fr.tatcher.combatminions;
 
-import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
-{
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
+@Mod(modid = CombatMinions.MODID, name = CombatMinions.NAME, version = CombatMinions.VERSION)
+public class CombatMinions {
+    public static final String MODID = "combatminions";
+    public static final String NAME = "Combat Minions";
     public static final String VERSION = "1.0";
 
     private static Logger logger;
@@ -23,9 +21,6 @@ public class ExampleMod
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    public void init(FMLInitializationEvent event){
     }
 }
