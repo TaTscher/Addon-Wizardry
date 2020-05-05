3c26eaddb3a5b835b533c9294b33d815d7db9dbd
Dyskal <yoshilexis@gmail.com>
2020-05-05 17:52:23 +0200

    update project

--- src/main/java/fr/tatscher/combatminions/CombatMinions.java
+++ src/main/java/fr/tatscher/combatminions/CombatMinions.java
@@ -9,8 +9,8 @@ import org.apache.logging.log4j.Logger;
 @Mod(modid = CombatMinions.MODID, name = CombatMinions.NAME, version = CombatMinions.VERSION)
 public class CombatMinions {
     public static final String MODID = "combatminions";
-    public static final String NAME = "Combat Minions";
-    public static final String VERSION = "1.0";
+    public static final String NAME = "Magical Combat Minions";
+    public static final String VERSION = "0.1-alpha";
 
     private static Logger logger;
 
